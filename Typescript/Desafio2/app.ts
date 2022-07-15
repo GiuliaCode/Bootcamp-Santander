79412761f04bb1f673bf4598367327bd5b4cdee1
Giulia Costa <101359720+GiuliaCode@users.noreply.github.com>
2022-07-15 17:51:16 -0300

    Add files via upload

---
+++ Typescript/Desafio2/app.ts
@@ -0,0 +1,63 @@
+/* Exerício 
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
+*/
+
+/* Solução */
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: Profissao
+  }
+  
+  enum Profissao {
+    Atriz,
+    Padeiro,
+  }
+  
+  
+  const maria: Pessoa = {
+    nome: 'maria',
+    idade: 29,
+  }
+  
+  const roberto: Pessoa = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Profissao.Padeiro
+  }
+  
+  const laura: Pessoa = {
+    nome: 'laura',
+    idade: 32,
+    profissao: Profissao.Atriz
+  }
+  
+  const carlos: Pessoa = {
+    nome: 'carlos',
+    idade: 19,
+    profissao: Profissao.Padeiro
+  }
+  
