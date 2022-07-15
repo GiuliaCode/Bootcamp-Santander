876b3923c3b7e566b8ce4324b6aa7beed5e0cd66
Giulia Costa <101359720+GiuliaCode@users.noreply.github.com>
2022-07-15 17:46:34 -0300

    Add files via upload

---
+++ Typescript/Desafio1/app.ts
@@ -0,0 +1,14 @@
+/* Exercício
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+
+/* Solução */
+const employee = {
+    code: 10,
+    name: "John",
+  }
